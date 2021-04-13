8062cfd5556b530ff65f0df518cfac1539a6c42a
Konrad Bochnia <kbochnia@gmail.com>
2021-04-13 18:39:11 +0200

    Fix bug with YCbCr conversion

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,12 @@ struct RGB((u8, u8, u8));
 
 impl RGB {
     fn to_ycbcr(&self) -> YCbCr {
-        let r = self.0 .0;
-        let g = self.0 .1;
-        let b = self.0 .2;
+        let (r, g, b) = self.0;
+        let (r, g, b) = (r as f32, g as f32, b as f32);
 
-        let y = 0.299 * r as f32 + 0.587 * g as f32 + 0.114 * b as f32;
-        let cb = 128.0 - 0.168736 * r as f32 + 0.331264 * g as f32 + 0.5 * b as f32;
-        let cr = 128.0 + 0.5 * r as f32 + 0.418688 * g as f32 + 0.081312 * b as f32;
+        let y = 0.299 * r + 0.587 * g + 0.114 * b;
+        let cb = 128.0 - 0.168736 * r - 0.331264 * g + 0.5 * b;
+        let cr = 128.0 + 0.5 * r - 0.418688 * g - 0.081312 * b;
         YCbCr((y as u8, cb as u8, cr as u8))
     }
 }
@@ -33,13 +32,12 @@ struct YCbCr((u8, u8, u8));
 
 impl YCbCr {
     fn to_rgb(&self) -> RGB {
-        let y = self.0 .0;
-        let cb = self.0 .1;
-        let cr = self.0 .2;
+        let (y, cb, cr) = self.0;
+        let (y, cb, cr) = (y as f32, cb as f32, cr as f32);
 
-        let r = y as f32 + 1.402 * (cr as f32 - 128.0);
-        let g = y as f32 - 0.344136 * (cb as f32 - 128.0) - 0.714136 * (cr as f32 - 128.0);
-        let b = y as f32 + 1.772 * (cb as f32 - 128.0);
+        let r = y + 1.402 * (cr - 128.0);
+        let g = y - 0.344136 * (cb - 128.0) - 0.714136 * (cr - 128.0);
+        let b = y + 1.772 * (cb - 128.0);
         RGB((r as u8, g as u8, b as u8))
     }
 }
@@ -107,17 +105,16 @@ pub fn load_img(file: web_sys::File) {
             let image_data = context.get_image_data(0.0, 0.0, 500.0, 500.0).unwrap();
             let data: Vec<u8> = image_data.data().to_vec();
 
-            let new_image_data =
-                web_sys::ImageData::new_with_u8_clamped_array(wasm_bindgen::Clamped(&data), 500)
-                    .unwrap();
-            context.put_image_data(&new_image_data, 0.0, 0.0).unwrap();
+            context.put_image_data(&image_data, 0.0, 0.0).unwrap();
             STATE.lock().unwrap().image_data = data;
             get_ycbcr();
         }) as Box<dyn FnMut()>);
+
         image.set_onload(Some(imageonload.as_ref().unchecked_ref()));
-        image.set_src(&src);
         imageonload.forget();
+        image.set_src(&src);
     }) as Box<dyn FnMut(_)>);
+
     fr.set_onload(Some(onload.as_ref().unchecked_ref()));
     onload.forget();
     fr.read_as_data_url(&file).unwrap();
