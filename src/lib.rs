ea2b18bb44bbbf0f07da9c06654886134134cc4a
Konrad Bochnia <kbochnia@gmail.com>
2021-04-13 19:01:51 +0200

    Add linebreak

--- src/lib.rs
+++ src/lib.rs
@@ -52,18 +52,7 @@ static STATE: Lazy<Mutex<JPEGState>> = Lazy::new(|| Mutex::new(Default::default(
 
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
-
-    body.append_child(&val)?;
-
     console_error_panic_hook::set_once();
-
     Ok(())
 }
 
