1b82be7a2bd9068151742f92f85a08eb3cae5ef6
Xert <xertdev@gmail.com>
2021-12-05 15:05:52 +0100

    fmt fix

--- src/section/g711_visualization/page.rs
+++ src/section/g711_visualization/page.rs
@@ -79,6 +79,7 @@ pub fn init(url: Url) -> Option<Model> {
     })
 }
 
+#[allow(unused_must_use)]
 pub fn deinit(model: &mut Model) {
     if model.audio_context.is_some() {
         model.audio_context.as_mut().unwrap().close().unwrap();
